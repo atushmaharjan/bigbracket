1c5f003b584b894746843246361a8ebb262dd0c2
Atush Maharjan <atoosmhn@gmail.com>
2020-10-28 09:14:03 +0545

    ToDo list Added

---
+++ ToDo list/main.js
@@ -0,0 +1,20 @@
+let addToDoButton=document.getElementById('addToDo');
+let todoContainer=document.getElementById('todocontainer');
+let inputField=document.getElementById('inputField');
+
+addToDoButton.addEventListener('click',function(){
+    var paragraph=document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText=inputField.value;
+    todoContainer.appendChild(paragraph);
+    inputField.value="";
+
+    paragraph.addEventListener('click',function(){
+        paragraph.style.textDecoration="line-through";
+    })
+
+    paragraph.addEventListener('dblclick', function(){
+        todoContainer.removeChild(paragraph);
+    })
+}
+)

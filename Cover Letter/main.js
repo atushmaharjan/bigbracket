fa555e42cbf7cb3d1a29d387b158d09405feb778
Atush Maharjan <atoosmhn@gmail.com>
2020-11-09 06:13:29 +0545

    Added redirect page

---
+++ Cover Letter/main.js
@@ -0,0 +1,7 @@
+ 
+window.onload = function(){
+    document.getElementById("btn").addEventListener("click", myFunction);
+       function myFunction() {
+        window.location.href = "http://www.w3schools.com";
+       }
+}

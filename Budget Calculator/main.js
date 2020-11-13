ce0ed994ecc79f8b6d4762f533056dcf339dc24e
Atush Maharjan <atoosmhn@gmail.com>
2020-11-13 10:57:33 +0545

    Added js

---
+++ Budget Calculator/main.js
@@ -0,0 +1,22 @@
+var budget = 0
+var balance = 0
+var expense = 0
+var count = 0 
+
+function addBudget(){
+    budget = document.getElementById('budgetinput').value
+    document.getElementById('budget-amount').innerText = budget
+}
+
+function addExpense(){
+    expense = parseInt(document.getElementById('amountinput').value)
+    budget = parseInt(document.getElementById('budget-amount').innerText)
+    if(count===0){
+        balance = budget - expense
+        count++
+    }else{
+        balance = balance - expense
+    }
+    document.getElementById('expense-amount').innerText = expense
+    document.getElementById('balance-amount').innerText= balance
+}
